a1982c56971f4b75c4b9c5942cdf129f2b71ddf4
micheleantonazzi <m7242872428>
2021-04-07 14:24:56 +0200

    color.py: moved to utilities folder

--- tests/test_color.py
+++ tests/test_color.py
@@ -1,9 +1,10 @@
-from gibson_dataset.color import Color
+from gibson_dataset.utilities.color import Color
 
 
 def test_color_rgb():
     color = Color(red=1, green=2, blue=3)
     assert color.RGB() == [1, 2, 3]
+
 
 def test_color_bgr():
     color = Color(red=1, green=2, blue=3)
