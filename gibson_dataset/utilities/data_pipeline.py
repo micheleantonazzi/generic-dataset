b009eb63c718e8eff87070472e355671cdf255a6
micheleantonazzi <m7242872428>
2021-04-08 15:04:09 +0200

    data_pipeline.py: added cuda stream to make operation async

--- gibson_dataset/utilities/data_pipeline.py
+++ gibson_dataset/utilities/data_pipeline.py
@@ -1,4 +1,6 @@
 import queue
+from typing import Callable
+
 import numpy as np
 import gibson_dataset.utilities.engine_selector as eg
 
@@ -8,17 +10,26 @@ class DataPipeline:
     This class constructs an elaboration pipeline to elaborate a single data.
     A pipeline is composed by a series of consecutive operations performed to the same data.
     A pipeline can be executed in CPU or GPU (using CuPy)
+    :
     """
 
-    def __init__(self, data: np.array, use_gpu: bool):
+    def __init__(self, data: np.array, use_gpu: bool, end_function: Callable[[np.array], np.array] = lambda data: data):
+        """
+        Creates a new pipeline
+        :param data: the data to be modified
+        :param use_gpu: indicates of the pipeline must be executed in gpu
+        :param end_function: a function which can be used to automatically save the data outside the pipeline
+        """
         self._data = data
         self._use_gpu = use_gpu
+        self._end_function = end_function
         self._operations = queue.Queue()
 
         self._xp = eg.get_engine(eg.NUMPY if not use_gpu else eg.CUPY)
 
         # IF the pipeline is executed in gpu, transfer data to device mandatory
         if use_gpu:
+            self._cuda_stream = self._xp.cuda.Stream(non_blocking=True)
             self._transfer_data_to_device()
 
     def _transfer_data_to_device(self) -> 'DataPipeline':
@@ -37,23 +48,43 @@ class DataPipeline:
         :return: return the pipeline
         :rtype DataPipeline
         """
-        self._operations.put(lambda data: self._xp.asnumpy(data))
+        self._operations.put(lambda data: self._xp.asnumpy(data, stream=self._cuda_stream))
 
         return self
 
-    def run(self) -> np.array:
+    def run(self) -> 'DataPipeline':
         """
-        Runs the pipeline
-        :return: None
+        Runs the pipeline returning the modified data.
+        Note that if the pipeline uses GPU, this is an ASYNC operation.
+        :return: the modified data
+        :rtype np.array
         """
         if self._use_gpu:
             self._get_data_from_device()
 
         ret = self._data
+
+        if self._use_gpu:
+            self._cuda_stream.use()
         for operation in self._operations.queue:
             ret = operation(ret)
 
-        return ret
+        self._data = ret
+
+        return self
+
+    def obtain_data(self) -> np.array:
+        """
+        Returns the data after the pipeline execution finishes and call the end-function specified in the constructor.
+        This a SYNC operation, so if the pipeline is running using GPU,
+        the current thread is blocked until all operations in the pipeline are completed.
+        :return: np.array
+        :rtype np.array
+        """
+        if self._use_gpu:
+            self._cuda_stream.synchronize()
+
+        return self._end_function(self._data)
 
     def convert_bgr_to_rgb(self) -> 'DataPipeline':
         """
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -6,7 +6,7 @@ from gibson_dataset.utilities.data_pipeline import DataPipeline
 def test_pipeline_bgr_to_rgb():
     image_bgr = np.tile([[2, 1, 0]], (256, 256)).reshape((256, 256, 3))
     rgb_image = np.tile([0, 1, 2], (256, 256)).reshape((256, 256, 3))
-    converted = DataPipeline(data=image_bgr, use_gpu=False).convert_bgr_to_rgb().run()
+    converted = DataPipeline(data=image_bgr, use_gpu=False).convert_bgr_to_rgb().run().obtain_data()
 
     assert np.array_equal(converted, rgb_image)
 
@@ -14,6 +14,6 @@ def test_pipeline_bgr_to_rgb():
 def test_pipeline_rgb_to_bgr():
     image_rgb = np.tile([[0, 1, 2]], (256, 256)).reshape((256, 256, 3))
     bgr_image = np.tile([2, 1, 0], (256, 256)).reshape((256, 256, 3))
-    converted = DataPipeline(data=image_rgb, use_gpu=False).convert_rgb_to_bgr().run()
+    converted = DataPipeline(data=image_rgb, use_gpu=False).convert_rgb_to_bgr().run().obtain_data()
 
     assert np.array_equal(converted, bgr_image)
