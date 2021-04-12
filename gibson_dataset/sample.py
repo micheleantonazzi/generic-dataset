80166591082c557765d11d1f77a8921d9270369c
micheleantonazzi <m7242872428>
2021-04-12 09:19:44 +0200

    sample.py: fixed typo

--- gibson_dataset/sample.py
+++ gibson_dataset/sample.py
@@ -2,20 +2,20 @@ import numpy as np
 from gibson_dataset.utilities.data_pipeline import DataPipeline
 from typing import Dict, Union, TypeVar, Callable, Any
 from threading import Lock
-from functools import update_wrapper, wraps
+from functools import wraps
 
 AnotherActivePipelineException = Exception
 
 TCallable = TypeVar('TCallable', bound=Callable[..., Any])
 
 
-def synchronized_on_field(field_name: str, raise_expection: bool = True) -> Callable[[TCallable], TCallable]:
+def synchronized_on_field(field_name: str, raise_exception: bool = True) -> Callable[[TCallable], TCallable]:
     def decorator(method: TCallable) -> TCallable:
         @wraps(method)
         def sync_method(self, *args, **kwargs):
             lock = self._locks[field_name]
             with lock:
-                if raise_expection and self._pipelines[field_name] is not None:
+                if raise_exception and self._pipelines[field_name] is not None:
                     raise AnotherActivePipelineException('Be careful, there is another active pipeline for {0}, please terminate it.'.format(field_name))
                 return method(self, *args, **kwargs)
         return sync_method
@@ -36,7 +36,7 @@ class Sample:
         self._locks: Dict[str, Lock] = {'color_image': Lock(), 'depth_image': Lock(), 'depth_data': Lock()}
         self._pipelines: Dict[str, Union[DataPipeline, None]] = {'color_image': None, 'depth_image': None, 'depth_data': None}
 
-    @synchronized_on_field(field_name='color_image', raise_expection=True)
+    @synchronized_on_field(field_name='color_image', raise_exception=True)
     def create_pipeline_for_color_image(self, use_gpu: bool = False) -> DataPipeline:
         """
         Create and return a new pipeline to elaborate color_image. If there is another active pipeline, raise an AnotherActivePipelineException
@@ -54,7 +54,7 @@ class Sample:
         self._pipelines['color_image'] = DataPipeline(data=self._color_image, use_gpu=use_gpu, end_function=assign)
         return self._pipelines['color_image']
 
-    @synchronized_on_field(field_name='depth_data', raise_expection=True)
+    @synchronized_on_field(field_name='depth_data', raise_exception=True)
     def create_pipeline_for_depth_data(self, use_gpu: bool = False) -> DataPipeline:
         """
         Create and return a new pipeline to elaborate depth data. If there is another active pipeline, raise an AnotherActivePipelineException.
@@ -73,7 +73,7 @@ class Sample:
         self._pipelines['depth_data'] = DataPipeline(data=self._depth_data, use_gpu=use_gpu, end_function=assign)
         return self._pipelines['depth_data']
 
-    @synchronized_on_field(field_name='depth_image', raise_expection=True)
+    @synchronized_on_field(field_name='depth_image', raise_exception=True)
     def create_pipeline_for_depth_image(self, use_gpu: bool = False) -> DataPipeline:
         """
         Create and return a new pipeline to elaborate depth image. If there is another active pipeline, raise an AnotherActivePipelineException.
@@ -92,7 +92,7 @@ class Sample:
         self._pipelines['depth_image'] = DataPipeline(data=self._depth_image, use_gpu=use_gpu, end_function=assign)
         return self._pipelines['depth_image']
 
-    @synchronized_on_field(field_name='depth_image', raise_expection=True)
+    @synchronized_on_field(field_name='depth_image', raise_exception=True)
     def create_pipeline_to_generate_depth_image(self, limit: float = 10, use_gpu: bool = False) -> DataPipeline:
         """
         Creates a pipeline to generate depth image starting from the depth data.
@@ -121,7 +121,7 @@ class Sample:
 
         return self._pipelines['depth_image']
 
-    @synchronized_on_field(field_name='color_image', raise_expection=True)
+    @synchronized_on_field(field_name='color_image', raise_exception=True)
     def get_color_image(self) -> np.ndarray:
         """
         Return color_image. If there is an active pipeline for this field, raises an AnotherActivePipelineException.
@@ -130,7 +130,7 @@ class Sample:
         """
         return self._color_image
 
-    @synchronized_on_field(field_name='depth_data', raise_expection=True)
+    @synchronized_on_field(field_name='depth_data', raise_exception=True)
     def get_depth_data(self) -> np.ndarray:
         """
         Return depth data. If there is an active pipeline for this field, raises an AnotherActivePipelineException.
@@ -139,7 +139,7 @@ class Sample:
         """
         return self._depth_data
 
-    @synchronized_on_field(field_name='depth_image', raise_expection=True)
+    @synchronized_on_field(field_name='depth_image', raise_exception=True)
     def get_depth_image(self) -> np.ndarray:
         """
         Return depth_image. If there is an active pipeline for this field, raises an AnotherActivePipelineException.
@@ -148,7 +148,7 @@ class Sample:
         """
         return self._depth_image
 
-    @synchronized_on_field(field_name='color_image', raise_expection=False)
+    @synchronized_on_field(field_name='color_image', raise_exception=False)
     def get_pipeline_color_image(self) -> DataPipeline:
         """
         Returns the color_image's pipeline
@@ -157,7 +157,7 @@ class Sample:
         """
         return self._pipelines['color_image']
 
-    @synchronized_on_field(field_name='depth_image', raise_expection=False)
+    @synchronized_on_field(field_name='depth_image', raise_exception=False)
     def get_pipeline_depth_image(self) -> DataPipeline:
         """
         Returns the depth_image's pipeline
@@ -166,7 +166,7 @@ class Sample:
         """
         return self._pipelines['depth_image']
 
-    @synchronized_on_field(field_name='depth_data', raise_expection=False)
+    @synchronized_on_field(field_name='depth_data', raise_exception=False)
     def get_pipeline_depth_data(self) -> DataPipeline:
         """
         Returns the depth_data's pipeline
