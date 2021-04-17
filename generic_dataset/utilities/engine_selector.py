f500135bccad896d3c4f9b4945b127ea684a390e
micheleantonazzi <m7242872428>
2021-04-17 12:23:37 +0200

    engine_selector.py: now if cupy don't available print an error message and not raises an exception

--- generic_dataset/utilities/engine_selector.py
+++ generic_dataset/utilities/engine_selector.py
@@ -34,9 +34,12 @@ def get_engine(engine: Engine):
     else:
         try:
             import cupy
-            if cupy.cuda.runtime.getDeviceCount() == 0:
-                print(colored('Sorry, you want to use GPU but you can\'t. You have CuPy correctly installed but not an available GPU', 'yellow'))
-                return numpy
             return cupy
-        except Exception as e:
-            raise Exception('Something went wrong, you cant use GPU :( \n' + str(e))
+        except ModuleNotFoundError as e:
+            print(colored('You can\'t use GPU because CUDA Toolkit is not correctly installed or there is no available GPU.\n'
+                          'To solve this issue follow these steps:\n'
+                          ' - Install CUDA Toolkit and set the environment variables\n'
+                          ' - Check if a GPU is available typing `nvidia-smi`\n'
+                          ' - Reinstall this package or install CuPy independently\n' + str(e), 'red'))
+            return numpy
+
