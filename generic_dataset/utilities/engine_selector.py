8189c814c4205fe036f53de9d0e127446c15b0be
micheleantonazzi <m7242872428>
2021-04-17 12:24:29 +0200

    engine_selector.py: now if cupy don't available print an error message and not raises an exception

--- generic_dataset/utilities/engine_selector.py
+++ generic_dataset/utilities/engine_selector.py
@@ -13,18 +13,18 @@ def check_cuda_support():
     """
     try:
         import cupy as cp
-        if cp.cuda.runtime.getDeviceCount() == 0:
-            print(colored('Sorry, you want to use GPU but you can\'t. You have CuPy correctly installed but not an available GPU :/', 'yellow'))
-        else:
-            print(colored('Success: you have CUDA Toolkit correctly installed and an available GPU, so you can use CuPy :)', 'green'))
+        print(colored('Success: you have CUDA Toolkit correctly installed and an available GPU, so you can use CuPy :)', 'green'))
     except Exception as e:
-        print('Something went wrong, you cant use CuPy :( \n' + str(e))
+        print(colored('You can\'t use GPU because CUDA Toolkit is not correctly installed or there is no available GPU.\n'
+                      'To solve this issue follow these steps:\n'
+                      ' - Install CUDA Toolkit and set the environment variables\n'
+                      ' - Check if a GPU is available typing `nvidia-smi`\n'
+                      ' - Reinstall this package or install CuPy independently\n' + str(e), 'red'))
 
 
 def get_engine(engine: Engine):
     """
     Returns the engine (CuPy or NumPy) checking the system configuration
-    :raise Exception if cupy is not supported
     :param engine: the engine to return
     :type engine: Engine
     :return:
@@ -35,7 +35,7 @@ def get_engine(engine: Engine):
         try:
             import cupy
             return cupy
-        except ModuleNotFoundError as e:
+        except Exception as e:
             print(colored('You can\'t use GPU because CUDA Toolkit is not correctly installed or there is no available GPU.\n'
                           'To solve this issue follow these steps:\n'
                           ' - Install CUDA Toolkit and set the environment variables\n'
