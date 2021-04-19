1cbed259e7170034b10cd7c5e905c63161e59697
micheleantonazzi <m7242872428>
2021-04-19 10:13:06 +0200

    gibson_dataset.py: added GibsonSample class and its tests

---
+++ generic_dataset/gibson_dataset.py
@@ -0,0 +1,8 @@
+from generic_dataset.sample_generator import SampleGenerator
+import numpy as np
+
+GibsonSample = SampleGenerator(name='GibsonSample')\
+    .add_field(field_name='depth_data', field_type=np.ndarray, add_to_dataset=True)\
+    .add_field(field_name='depth_image', field_type=np.ndarray, add_to_dataset=True)\
+    .add_field(field_name='color_image', field_type=np.ndarray, add_to_dataset=True)\
+    .generate_sample_class()
---
+++ tests/test_gibson_dataset.py
@@ -0,0 +1,7 @@
+import numpy as np
+from generic_dataset.gibson_dataset import GibsonSample
+
+
+def test_setters():
+    gibson_sample = GibsonSample()
+    gibson_sample.set_depth_data(value=np.array([])).set_depth_image(value=np.array([])).set_color_image(value=np.array([]))
