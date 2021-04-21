cf6a51b0d6c1d8e04c8b678c3dcba24a65171605
micheleantonazzi <m7242872428>
2021-04-21 17:46:30 +0200

    test_dataset_disk_manager.py: fixed

--- tests/test_dataset_disk_manager.py
+++ tests/test_dataset_disk_manager.py
@@ -1,9 +1,12 @@
 import os
 
 import numpy as np
+import pytest
+
 from generic_dataset.data_pipeline import DataPipeline
 from generic_dataset.dataset_disk_manager import DatasetDiskManager
 from generic_dataset.sample_generator import SampleGenerator
+
 
 pipeline_field_1_2 = DataPipeline().add_operation(lambda data, engine: (engine.array([1 for i in range(10000)]), engine))
 GeneratedSample = SampleGenerator(name='GeneratedSample').add_field(field_name='field_1', field_type=np.ndarray, add_to_dataset=True) \
@@ -14,6 +17,9 @@ GeneratedSample = SampleGenerator(name='GeneratedSample').add_field(field_name='
 
 
 def test_constructor():
+    with pytest.raises(FileNotFoundError):
+        DatasetDiskManager(dataset_path='random_path', folder_name='folder', sample=GeneratedSample(is_positive=False))
+
     path = os.path.join(os.path.dirname(__file__), 'dataset_folder')
     DatasetDiskManager(dataset_path=path, folder_name='folder', sample=GeneratedSample(is_positive=False))
     DatasetDiskManager(dataset_path=path, folder_name='folder1', sample=GeneratedSample(is_positive=False))
