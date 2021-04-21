de3aa389bcaf14da0738c26c98fb29caafe38ad1
micheleantonazzi <m7242872428>
2021-04-21 18:21:36 +0200

    save_load_methods.py: added new methods and tests

--- generic_dataset/utilities/save_load_methods.py
+++ generic_dataset/utilities/save_load_methods.py
@@ -1,7 +1,9 @@
 """
 This file contains a lot of method to save and load data from disk
 """
-from typing import NoReturn
+import gzip
+import json
+from typing import NoReturn, Dict
 
 import cv2
 import numpy as np
@@ -13,3 +15,23 @@ def save_cv2_image(path: str, data: np.ndarray) -> NoReturn:
 
 def load_cv2_image(path: str) -> np.ndarray:
     return cv2.imread(path)
+
+
+def save_dictionary_compressed(path: str, data: Dict) -> NoReturn:
+    with gzip.open(path, mode='w') as file:
+        file.write(json.dumps(data).encode('utf-8'))
+
+
+def load_dictionary_compressed(path: str) -> Dict:
+    with gzip.open(path, mode='r') as file:
+        return json.loads(file.read().decode('utf-8'))
+
+
+def save_compressed_numpy_array(path: str, data: np.ndarray) -> NoReturn:
+    with gzip.open(path, mode='w') as file:
+        np.save(file, arr=data)
+
+
+def load_compressed_numpy_array(path: str) -> np.ndarray:
+    with gzip.open(path, mode='r') as file:
+        return np.load(file)
--- tests/test_load_save_methods.py
+++ tests/test_load_save_methods.py
@@ -16,3 +16,24 @@ def test_cv2_save_load_methods():
     loaded_image = slm.load_cv2_image(path)
 
     assert np.array_equal(loaded_image, image)
+
+
+def test_save_load_dictionary():
+    d = {'field1': 1.1, 'field2': 'Hi', 'field3': [0, 1, 2]}
+    path = os.path.join(test_methods_path, 'dictionary.tar.gz')
+
+    slm.save_dictionary_compressed(path, d)
+    d_loaded = slm.load_dictionary_compressed(path)
+
+    assert d_loaded == d
+
+
+def test_save_numpy_array():
+    array = np.asarray([1.1 for _ in range(256*256)])
+
+    path = os.path.join(test_methods_path, 'numpy_array.tar.gz')
+
+    slm.save_compressed_numpy_array(path, array)
+    array_loaded = slm.load_compressed_numpy_array(path)
+    assert np.array_equal(array, array_loaded)
+
